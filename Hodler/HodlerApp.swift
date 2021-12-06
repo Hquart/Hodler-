b28e94cba7b72ec03323a943aa1ac19861135f1a
Hquart <hquart@icloud.com>
2021-12-06 19:24:40 +0100

    Initial Commit

---
+++ Hodler/HodlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HodlerApp.swift
+//  Hodler
+//
+//  Created by Naji Achkar on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HodlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
